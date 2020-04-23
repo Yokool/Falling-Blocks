6ed673a449868c59fbc5959796783060079eefa2
Yokool <letsgamecze@gmail.com>
2020-04-23 17:37:09 +0200

    fix

--- Assets/Scripts/ShopItems/Managers/ShopSaveSystem.cs
+++ Assets/Scripts/ShopItems/Managers/ShopSaveSystem.cs
@@ -38,8 +38,7 @@ public static class ShopSaveSystem
 
     private static void CreateItemManagers()
     {
-
-        ItemUpgradeManager Jetpack_Refuel_Manager = new ItemUpgradeManager("Jetpack Refuel pipes", "Upgrading the pipes.", 50, null, 1.1f, 9, new Jetpack_Refuel_Buy());
+        ItemUpgradeManager Jetpack_Refuel_Manager = new ItemUpgradeManager("Jetpack Refuel pipes", "Upgrading the pipes.", 50, SpriteDatabase.RefuelPipes, 1.1f, 9, new Jetpack_Refuel_Buy());
         ItemUpgradeManager Jetpack_SpendfuelDecrement_Manager = new ItemUpgradeManager("Jetpack engine efficiency", "Finding yourself with not enough air time? Boost your engine to stay in the air for longer.", 500, null, 1.1f, 50, new Jetpack_SpendfuelDecrement_Buy());
         ItemUpgradeManager Jetpack_Maxfuel_Manager = new ItemUpgradeManager("Jetpack fuel tank", "More tanks, more fuel.", 50, null, 1.1f, 9, new Jetpack_Maxfuel_Buy());
         ItemUpgradeManager Jetpack_Strength_Manager = new ItemUpgradeManager("Jetpack Engine", "Improving the jetpack engine will boost its upper momentum.", 40, null, 1.1f, 20, new Jetpack_Strength_Buy());
---
+++ Assets/Scripts/SpriteDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SpriteDatabase
+{
+
+    public static Sprite RefuelPipes;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+    public static void LoadSprites()
+    {
+        RefuelPipes = Resources.Load<Sprite>("ShopIcons/S_FuelPipe");
+        Debug.Log(RefuelPipes);
+    }
+
+}
