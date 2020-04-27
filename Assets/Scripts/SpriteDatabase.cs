a6061675fe53cbe4b8242302d792aede448a6549
Yokool <letsgamecze@gmail.com>
2020-04-27 12:46:28 +0200

    ShopIcons

--- Assets/Scripts/SpriteDatabase.cs
+++ Assets/Scripts/SpriteDatabase.cs
@@ -6,12 +6,19 @@ public static class SpriteDatabase
 {
 
     public static Sprite RefuelPipes;
+    public static Sprite Canister;
+    public static Sprite JetEngine;
+    public static Sprite GoldenMask;
+    public static Sprite MechanicalLeg;
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
     public static void LoadSprites()
     {
-        RefuelPipes = Resources.Load<Sprite>("ShopIcons/S_FuelPipe");
-        Debug.Log(RefuelPipes);
+        RefuelPipes = Resources.Load<Sprite>("ShopIcons/FuelPipe");
+        Canister = Resources.Load<Sprite>("ShopIcons/Canister");
+        JetEngine = Resources.Load<Sprite>("ShopIcons/JetEngine");
+        GoldenMask = Resources.Load<Sprite>("ShopIcons/GoldenMask");
+        MechanicalLeg = Resources.Load<Sprite>("ShopIcons/MechanicalLeg");
     }
 
 }
